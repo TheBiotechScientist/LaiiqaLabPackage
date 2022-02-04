6f178778767d6bc74e79eba1ab0274271059d44f
F. Javier M <fmoralesm87@gmail.com>
2022-02-03 22:44:16 -0600

    helpers.py -> Agregada y comentada función join_plots()

--- DataAnalysis/helpers.py
+++ DataAnalysis/helpers.py
@@ -1,5 +1,7 @@
 from os import getcwd, listdir, chdir
 import natsort as nsrt
+import pandas as pd
+import matplotlib.pyplot as plt
 
 figuras = r'C:\Users\javne\Dropbox\Tesis Maestria Javier\Tesis\Figuras'
 figuras_resultados = r'C:\Users\javne\Dropbox\Tesis Maestria Javier\Tesis\Figuras\resultados'
@@ -33,3 +35,21 @@ def save_plot(fig, name):
 def save_data(self, frame, name):
     frame.to_csv(name, index=frame.index)
     # frame.to_csv(name, index=self.index)
+
+# def join_plots(frames=list(), labels=list(),title='Cinética de Ozonización', subtitle='', xlabel='Tiempo ', ylabel='$O_3$ [$g/Nm^3$]', width=23, height=15, x0=0, xf=None, y0=-0.5, yf=35.5, time='seg', grid=True, visible=True, dx=1.0, **kwargs):
+#     wcm = cm2in(width)
+#     hcm = cm2in(height)
+#     fig, ax = plt.subplots(figsize=[wcm,hcm])
+#     ax.grid(grid)
+#     for frame,label in zip(frames,labels):
+#         frame.plot(y='conc', ax=ax, label=label)
+#
+#     plt.suptitle(title, fontsize=14)
+#     plt.title(subtitle, fontsize=12)
+#     plt.xlim(x0, xf)
+#     plt.ylim(y0, yf)
+#     plt.xlabel(xlabel+f'({time})', fontsize=12)
+#     plt.ylabel(ylabel, fontsize=12)
+#     plt.close()
+#     joined_plots = fig.get_figure()
+#     return joined_plots
