2f057664392f7bea7f73481aa97ada65550b93f0
F. Javier Morales M <fmoralesm87@gmail.com>
2022-02-13 16:57:09 -0600

    helpers.py -> Agregada la función join_plots().

--- build/lib/DataAnalysis/helpers.py
+++ build/lib/DataAnalysis/helpers.py
@@ -1,14 +1,24 @@
-from os import *
+from os import getcwd, listdir, chdir
 import natsort as nsrt
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def dir():
-    return getcwd()
+figuras = r'C:\Users\javne\Dropbox\Tesis Maestria Javier\Tesis\Figuras'
+figuras_resultados = r'C:\Users\javne\Dropbox\Tesis Maestria Javier\Tesis\Figuras\resultados'
+experimentacion = r'C:\Users\javne\Dropbox\Tesis Maestria Javier\Tesis\Experimentacion'
+experimentacion_ozonaciones = r'C:\Users\javne\Dropbox\Tesis Maestria Javier\Tesis\Experimentacion\O3onaciones'
+experimentacion_espectrouv = r'C:\Users\javne\Dropbox\Tesis Maestria Javier\Tesis\Experimentacion\EspectroUV'
+
+def where():
+    w = getcwd()
+    return print(w), nsrt.natsorted(listdir(w))
 
 
 
 def cd(folder):
     chdir(folder)
-    return nsrt.natsorted(listdir(getcwd()))
+    d = getcwd()
+    return print(d), nsrt.natsorted(listdir(d))
 
 
 
@@ -25,3 +35,21 @@ def save_plot(fig, name):
 def save_data(self, frame, name):
     frame.to_csv(name, index=frame.index)
     # frame.to_csv(name, index=self.index)
+
+# def join_plots(frames=list(), labels=list(),title='Cinética de Ozonización', subtitle='', xlabel='Tiempo ', ylabel='$O_3$ [$g/Nm^3$]', width=23, height=15, x0=0, xf=None, y0=-0.5, yf=35.5, time='seg', grid=True, visible=True, dx=1.0, **kwargs):
+#     wcm = cm2in(width)
+#     hcm = cm2in(height)
+#     fig, ax = plt.subplots(figsize=[wcm,hcm])
+#     ax.grid(grid)
+#     for frame,label in zip(frames,labels):
+#         frame.plot(y='conc', ax=ax, label=label)
+#
+#     plt.suptitle(title, fontsize=14)
+#     plt.title(subtitle, fontsize=12)
+#     plt.xlim(x0, xf)
+#     plt.ylim(y0, yf)
+#     plt.xlabel(xlabel+f'({time})', fontsize=12)
+#     plt.ylabel(ylabel, fontsize=12)
+#     plt.close()
+#     joined_plots = fig.get_figure()
+#     return joined_plots
