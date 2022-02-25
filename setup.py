af36db2eb0110f91ef8ae2074ccab3139da0a96b
F. Javier Morales M <fmoralesm87@gmail.com>
2022-02-25 16:14:15 -0600

    Iniciando versión 0.1.1. Tkinter para seleccionar multiples archivos

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding="utf-8") as f:
 
 setup(
 name = 'laiiqa-lab-utilities',
-version = '0.1.0',
+version = '0.1.1',
 description = 'Package for ozonation, spectro uv and bioreaction data analysis files for the LAIIQA laboratory (ESIQIE - IPN).',
 long_description = long_description,
 long_description_content_type = 'text/markdown',
